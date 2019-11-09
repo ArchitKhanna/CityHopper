6d7f22cbf08be181e06aec1cca5ed2fa4daa1340
David-Bromell <davidbromell62@gmail.com>
2019-11-09 17:43:46 +0000

    Login/Logout functionality
    Login/Logout page now works
    when login you are redirected to home page
    
    Navbar also changes depending on if you are logged in or out
    
    When logout clicked you are brought to logout page and a hyperlink called signin is below to bring you back to login page..

--- CityHopper/UsersApp/views.py
+++ CityHopper/UsersApp/views.py
@@ -7,11 +7,12 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('cityhopper-home')
+            messages.success(request, f'Account created successfully!')
+            return redirect('login')
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+
 
     #message.debug
     #message.info
