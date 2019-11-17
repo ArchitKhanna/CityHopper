b2f854a9b227b609ea757198b3e2b0dce71f0c70
Archit Khanna <architk.222@gmail.com>
2019-11-17 19:29:39 +0000

    Added Validation for Booking Form
    - Forms now validates date and number of tickets;
    - No Error displayed on entering invalid values to submit;

--- CityHopper/UsersApp/models.py
+++ CityHopper/UsersApp/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg',upload_to='profile_pics')
-def __str__(self):
+
+    def __str__(self):
         return f'{self.user.username} Profile'
+
+class Bookings(models.Model):
+    startinglocation = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+    journeydate = models.DateField()
+    starttime = models.TimeField()
+    journeytype = models.CharField(max_length=100)
+    numberoftickets = models.IntegerField()
 # Create your models here.
