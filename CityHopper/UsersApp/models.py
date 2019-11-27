b84afb574337673908c454b68312d5f3a6b12b3d
18159761 <48758161+18159761@users.noreply.github.com>
2019-11-27 21:19:21 +0000

    Contact Class
    Updating contact Class

--- CityHopper/UsersApp/models.py
+++ CityHopper/UsersApp/models.py
@@ -14,7 +14,11 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    message = models.TextField()
 
 """class Bookings(models.Model):
     startinglocation = models.CharField(max_length=100)
